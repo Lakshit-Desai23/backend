62c0dd1bbce450df001ceee754dbc03c8c529bd0
Lakshit-Desai23 <141605695+Lakshit-Desai23@users.noreply.github.com>
2026-03-26 00:48:16 +0530

    new change

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 import sys
 
@@ -10,4 +11,9 @@ import uvicorn
 
 
 if __name__ == "__main__":
-    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=False)
+    uvicorn.run(
+        "app.main:app",
+        host="0.0.0.0",
+        port=int(os.getenv("PORT", "8000")),
+        reload=False,
+    )
